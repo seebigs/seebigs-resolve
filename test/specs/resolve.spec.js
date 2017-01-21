b8a41bc3644b54ef65dd2bc1cb9cb46ba2b5eb69
Chris Bigelow <seebigs@gmail.com>
2017-01-21 02:55:41 -0500

    CB: add specs for loadLocal from package.json

--- test/specs/resolve.spec.js
+++ test/specs/resolve.spec.js
@@ -4,18 +4,31 @@ var resolve = require('../../index.js');
 
 var fromFile = ('./test/specs/resolve.spec.js');
 var absOne = path.resolve(__dirname + '/../fixture/index.js');
+var absMain = path.resolve(__dirname + '/../fixture/main/lib/main.js');
 
 describe('resolve', function () {
 
     describe('loadLocal', function (expect) {
-        var loc = {
-            contents: "\nmodule.exports = { name: 'ONE' };\n",
-            path: absOne
-        };
-        expect(resolve(absOne), fromFile).toBe(loc);
-        expect(resolve('../fixture/index.js', fromFile)).toBe(loc);
-        expect(resolve('../fixture/index', fromFile)).toBe(loc);
-        expect(resolve('../fixture', fromFile)).toBe(loc);
+
+        describe('by index', function (expect) {
+            var loc = {
+                contents: "\nmodule.exports = { name: 'ONE' };\n",
+                path: absOne
+            };
+            expect(resolve(absOne), fromFile).toBe(loc);
+            expect(resolve('../fixture/index.js', fromFile)).toBe(loc);
+            expect(resolve('../fixture/index', fromFile)).toBe(loc);
+            expect(resolve('../fixture', fromFile)).toBe(loc);
+        });
+
+        describe('by package.json', function (expect) {
+            var loc = {
+                contents: "\nmodule.exports = { name: 'MAIN' };\n",
+                path: absMain
+            };
+            expect(resolve('fixture/main', fromFile, ['./test'])).toBe(loc);
+        });
+
     });
 
     describe('loadModule', function (expect) {
